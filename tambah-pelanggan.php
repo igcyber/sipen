7993421b0ef455eb749bd590a268a3a76b26b9dd
irfangaim <irfan.gaim1@gmail.com>
2020-11-11 22:47:02 +0800

    bingung

--- tambah-pelanggan.php
+++ tambah-pelanggan.php
@@ -12,62 +12,6 @@
                 }
 		</script>
 <!-- Hanya Angka -->
-<?php 
-    if(isset($_POST['submit'])){
-        $rek_baru=$_POST['rek_baru'];
-        $rek_lama=$_POST['rek_lama'];
-        $nm_pelanggan=$_POST['nm_pelanggan'];
-        $id_unit=$_POST['id_unit'];
-        $kelurahan=$_POST['kelurahan'];
-        $kecamatan=$_POST['kecamatan'];
-        $id_kelompok=$_POST['id_kelompok'];
-        $status=$_POST['status'];
-        $tgl_status=$_POST['tgl_status'];
-        $no_petugas=$_POST['no_petugas'];
-        $nm_petugas=$_POST['nm_petugas'];
-        
-        $image=$_FILES['foto_rumah']['name'];
-        $image_tmp=$_FILES['foto_rumah']['tmp_name'];
-        $image_size=$_FILES['foto_rumah']['size'];
-        $image_ext=explode('.',$image);
-        $image_ext=strtolower(end($image_ext));
-        $image_new=uniqid().'.'.$image_ext;
-        $store="foto_rumah/".$image_new;
-
-        if ($image_ext=='jpg' || $image_ext=='jpeg' || $image_ext=='png' || $image_ext=='gif') {
-            if ($image_size >= 100000000000000000000000000000) {
-              $error="Error";
-              echo $error;
-            }
-            else{
-              if (move_uploaded_file($image_tmp, $store)) {
-                $foto=$image_new;
-                if(!isset($error))
-                {
-                    $insert=$pdo->prepare("INSERT INTO 
-                    pelanggan(rek_baru,rek_lama,nm_pelanggan,id_unit,kelurahan,kecamatan,id_kelompok,status,tgl_status,no_petugas,nm_petugas,foto_rumah)
-                    VALUES(:rek_baru,:rek_lama,:nm_pelanggan,:id_unit,:kelurahan,:kecamatan,:id_kelompok,:status,:tgl_status,:no_petugas,:nm_petugas,:foto_rumah)");
-
-                        $insert->bindParam(':rek_baru',$rek_baru);
-                        $insert->bindParam(':rek_lama',$rek_lama);
-                        $insert->bindParam(':nm_pelanggan',$nm_pelanggan);
-                        $insert->bindParam(':id_unit',$id_unit);
-                        $insert->bindParam(':kelurahan',$kelurahan);
-                        $insert->bindParam(':kecamatan',$kecamatan);
-                        $insert->bindParam(':id_kelompok',$id_kelompok);
-                        $insert->bindParam(':status',$status);
-                        $insert->bindParam(':tgl_status',$tgl_status);
-                        $insert->bindParam(':no_petugas',$no_petugas);
-                        $insert->bindParam(':nm_petugas',$nm_petugas);
-                        $insert->bindParam(':foto_rumah',$foto_rumah);
-                        $insert->execute();
-                }
-                }
-            }
-        }
-          
-    }
-?>
        <div id="layoutSidenav_content">
                 <main>
                     <div class="page-header pb-10 page-header-dark bg-gradient-primary-to-secondary">
@@ -86,7 +30,7 @@
                         <div class="card mb-4">
                             <div class="card-header"></div>
                             <div class="card-body">
-                                <form action="" method ="POST" >
+                                <form action="db_tambah_pelanggan.php" method ="POST" enctype="multipart/form-data">
                                     <div class="form-row">
                                         <div class="form-group col-md-6">
                                             <label for="post-title">Nomor Rekening Baru</label>
@@ -127,7 +71,7 @@
 
                                     <div class="form-group">
                                         <label for="post-content">Alamat Lengkap Pelanggan</label>
-                                        <textarea class="form-control" placeholder="Alamat Lengkap..." rows="9"></textarea>
+                                        <textarea class="form-control" placeholder="Alamat Lengkap..." rows="9" name="alamat"></textarea>
                                     </div>
 
                                     <div class="form-row">
@@ -193,22 +137,21 @@
                                         </div>
                                         <div class="form-group col-md-6">
                                             <label>Hasil Test Meteran</label>
-                                            <input class="form-control" type="text" placeholder="Hasil Test Meter..." />
+                                            <input class="form-control" type="text" name="hasil_test" placeholder="Hasil Test Meter..." />
                                         </div>
                                     </div>
 
                                     <div class="form-row">
                                         <div class="form-group col-md-6">
                                             <label>Tanggal Hasil Test</label>
-                                            <input class="form-control" type="date" placeholder="Tanggal Status..." />
+                                            <input class="form-control" type="date" name="tgl_hasil_test" placeholder="Tanggal Status..." />
                                         </div>
                                         <div class="form-group col-md-6">
                                             <label>Foto Rumah Pelanggan</label>
                                             <input type="file" class="form-control" name="foto_rumah">
                                         </div>
                                     </div>
-                                    <button type="submit" class="btn btn-info" name="submit">SIMPAN</button>
-                                    <!-- <button class="btn btn-primary mr-2 my-1" type="button" name="submit">Simpan</button> -->
+                                    <button class="btn btn-primary mr-2 my-1" type="submit" name="submit">Simpan</button>
                                     <a href="pelanggan.php" class="btn btn-secondary">Kembali</a>
                                 </form>
                             </div>
@@ -219,9 +162,11 @@
             </div>
 <?php require_once("includes/foot.php"); ?>
 
+<!-- No petugas otomatis -->
 <script type="text/javascript"> 
 <?php echo $jsArray; ?>
 function changeValue(id){
     document.getElementById('no_petugas').value = prdName[id].no_petugas;
 };
 </script>
+<!-- No petugas otomatis -->
