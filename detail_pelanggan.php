c36149a118e5984de691203539e442e5cc420d69
irfangaim <irfan.gaim1@gmail.com>
2020-11-15 12:59:59 +0800

    Progres Read Pelanggan Clear

--- detail_pelanggan.php
+++ detail_pelanggan.php
@@ -34,48 +34,89 @@
                         <div class="card mb-4">
                             <div class="card-body">
                                 <div class="datatable table-responsive">
-                                    <?php
+                                <?php
                                         $id=$_GET['id'];
-                                        $select=$pdo->prepare("SELECT * FROM pelanggan WHERE id=$id");
-                                        $select->execute();
-                                        while($row=$select->fetch(PDO::FETCH_OBJ)){
-                                    ?>
+                                        $sql=$pdo->prepare("SELECT * FROM pelanggan WHERE id=$id");
+                                        $sql->execute();
+                                        while($pelanggan = $sql->fetch(PDO::FETCH_ASSOC)){
+                                            $rek_baru = $pelanggan['rek_baru'];
+                                            $rek_lama = $pelanggan['rek_lama'];
+                                            $nm_pelanggan = $pelanggan['nm_pelanggan'];
+
+                                            //Mengambil Nama Wilayah Dari Tabel Unit
+                                            $unit_id = $pelanggan['unit_id'];
+                                            $sql_unit = "SELECT * FROM unit  WHERE id = :id";
+                                            $res_unit = $pdo->prepare($sql_unit);
+                                            $res_unit->execute([':id'=>$unit_id]);
+                                            $unit = $res_unit->fetch(PDO::FETCH_ASSOC);
+                                            $unit_nm = $unit['nm_wilayah'];
+
+                                            $alamat = $pelanggan['alamat'];
+                                            $kelurahan = $pelanggan['kelurahan'];
+                                            $kecamatan = $pelanggan['kecamatan'];
+
+                                            //Mengambil Nama & Kode Kelompok Dari Tabel Kelompok
+                                            $kelompok_id = $pelanggan['kelompok_id'];
+                                            $sql_kelompok = "SELECT * FROM kelompok WHERE id = :id";
+                                            $res_kelompok = $pdo->prepare($sql_kelompok);
+                                            $res_kelompok->execute([':id'=>$kelompok_id]);
+                                            $kelompok = $res_kelompok->fetch(PDO::FETCH_ASSOC);
+                                            $kd_kelompok = $kelompok['kd_kelompok'];
+                                            $nm_kelompok = $kelompok['nm_kelompok'];
+
+                                            $status = $pelanggan['status'];
+                                            $tgl_status = $pelanggan['tgl_status'];
+                                            $hasil_test = $pelanggan['hasil_test'];
+                                            $tgl_hasil_test = $pelanggan['tgl_hasil_test'];
+
+                                            //Mengambil Nama & Nomor Petugas Dari Tabel Petugas
+                                            $petugas_id = $pelanggan['petugas_id'];
+                                            $sql_petugas = "SELECT * FROM petugas WHERE id = :id";
+                                            $res_petugas = $pdo->prepare($sql_petugas);
+                                            $res_petugas->execute([':id'=>$petugas_id]);
+                                            $petugas = $res_petugas->fetch(PDO::FETCH_ASSOC);
+                                            $no_petugas = $petugas['no_petugas'];
+                                            $no_petugas_lama = $petugas['no_petugas_lama'];
+                                            $nm_petugas = $petugas['nm_petugas'];
+
+                                            $foto = $pelanggan['foto_rumah'];
+                                ?>
                                         <div class="col-md-12">
                                             <div class="row">
                                                 <div class="col-md-3">
-                                                    <p><img src="foto_rumah/<?php echo $row->foto_rumah ?>" alt="Rumah Pelanggan" style="width: 220px; height: 250px;" ></p>
-                                                        <label for="" >Nomor Rekening :   </label>
-                                                        <input type="text" value="<?php echo $row->rek_baru ?>" readonly>
+                                                    <p><img src="foto_rumah/<?php echo $foto ?>" alt="Rumah Pelanggan" style="width: 220px; height: 250px;" ></p>
+                                                        <label>Nomor Rekening :   </label>
+                                                        <input type="text" value="<?php echo $rek_baru ?>" readonly>
 
-                                                        <label for="" >Nama Pelanggan :</label>
-                                                        <input type="text" value="<?php echo $row->nm_pelanggan ?>" readonly>                                              
+                                                        <label>Nama Pelanggan :</label>
+                                                        <input type="text" value="<?php echo $nm_pelanggan ?>" readonly>                                              
                                                 </div>
                                                     <div class="form-group col-6">
-                                                        <form action="" >
+                                                        <form action="">
                                                             
-                                                            <input type="text" style="float:right; width: calc(100% - 200px);" value="<?php echo $row->unit_id ?>" readonly>
-                                                            <label for="">Wilayah</label><br>
+                                                            <input type="text" style="float:right; width: calc(100% - 200px);" value="<?php echo $unit_nm ?>" readonly>
+                                                            <label>Wilayah</label><br>
                                                             
-                                                            <input type="text" style="float:right; width: calc(100% - 200px);" value="<?php echo $row->alamat ?>" readonly>
-                                                            <label for="" ">Alamat</label><br>
+                                                            <input type="text" style="float:right; width: calc(100% - 200px);" value="<?php echo $alamat ?>" readonly>
+                                                            <label>Alamat</label><br>
                                                             
-                                                            <input type="text"style="float:right; width: calc(100% - 200px);" value="<?php echo $row->kelurahan ?>" readonly>
-                                                            <label for="">Kelurahan</label><br>
+                                                            <input type="text"style="float:right; width: calc(100% - 200px);" value="<?php echo $kelurahan ?>" readonly>
+                                                            <label>Kelurahan</label><br>
 
-                                                            <input type="text"style="float:right; width: calc(100% - 200px);" value="<?php echo $row->kecamatan ?>" readonly>
-                                                            <label for="">Kecamatan</label><br>
+                                                            <input type="text"style="float:right; width: calc(100% - 200px);" value="<?php echo $kecamatan ?>" readonly>
+                                                            <label>Kecamatan</label><br>
 
-                                                            <input type="text"style="float:right; width: calc(100% - 200px);"value="<?php echo $row->kelompok_id ?>" readonly>
-                                                            <label for="">Nama Kelompok</label><br>
+                                                            <input type="text"style="float:right; width: calc(100% - 200px);"value="<?php echo $kd_kelompok ?> - <?php echo $nm_kelompok ?>" readonly>
+                                                            <label>Nama Kelompok</label><br>
                                                             
-                                                            <input type="text"style="float:right; width: calc(100% - 200px);" value="<?php echo $row->status ?> /  <?php echo date("d F Y",strtotime($row->tgl_status)); ?>" readonly>
-                                                            <label for="">Status/Tanggal Status</label><br>
+                                                            <input type="text"style="float:right; width: calc(100% - 200px);" value="<?php echo $status ?> /  <?php echo date("d F Y",strtotime($tgl_status)); ?>" readonly>
+                                                            <label>Status/Tanggal Status</label><br>
 
-                                                            <input type="text"style="float:right; width: calc(100% - 200px);"value="<?php echo $row->hasil_test ?> /  <?php echo date("d F Y",strtotime($row->tgl_hasil_test)); ?>" readonly>
-                                                            <label for="">Hasil Test/Tanggal Hasil Test</label><br>
+                                                            <input type="text"style="float:right; width: calc(100% - 200px);"value="<?php echo $hasil_test ?> /  <?php echo date("d F Y",strtotime($tgl_hasil_test)); ?>" readonly>
+                                                            <label>Hasil Test/Tanggal Hasil Test</label><br>
 
-                                                            <input type="text"style="float:right; width: calc(100% - 200px);" value="<?php echo $row->petugas_id ?>" readonly>
-                                                            <label for="">Nama Petugas</label>
+                                                            <input type="text"style="float:right; width: calc(100% - 200px);" value="<?php echo $no_petugas ?> - <?php echo $nm_petugas ?>" readonly>
+                                                            <label>Nomor & Nama Petugas</label>
                                                         </form>
                                                     </div>
                                             </div>
--- tambah-pelanggan.php
+++ tambah-pelanggan.php
@@ -1,22 +1,18 @@
-<?php require_once("includes/head.php"); ?>
-       <?php require_once("includes/top-nav.php"); ?>
+<?php $current_page = "Tambah Pelanggan";?>
 
-<?php 
-//  Settingan waktu indonesia
-date_default_timezone_set('Asia/Makassar');
-// Settingan waktu indonesia
-?>
+<?php require_once("includes/head.php"); ?>
+<?php require_once("includes/top-nav.php") ?>
 
 <!-- Hanya Angka -->
-<script>
-    function hanyaAngka(evt){
-        var charCode = (evt.which) ? evt.which : event.keyCode
-        if (charCode > 31 && (charCode <48 || charCode > 57))
+        <script>
+            function hanyaAngka(evt){
+                var charCode = (evt.which) ? evt.which : event.keyCode
+                if (charCode > 31 && (charCode <48 || charCode > 57))
 
-            return false;
-            return true;
-        }
-</script>
+                    return false;
+                    return true;
+                }
+		</script>
 <!-- Hanya Angka -->
 
 
@@ -38,7 +34,7 @@ date_default_timezone_set('Asia/Makassar');
         $petugas_id = $_POST['petugas_id'];
         $foto_rumah = $_FILES['foto_rumah']['name'];
         $foto_rumah_tmp = $_FILES['foto_rumah']['tmp_name'];
-        move_uploaded_file("{$foto_rumah_tmp}","foto_rumah/{$foto_rumah}","$image_size >= 2048");
+        move_uploaded_file("{$foto_rumah_tmp}","foto_rumah/{$foto_rumah}");
 
         $sql = "INSERT INTO pelanggan(rek_baru,rek_lama,nm_pelanggan,unit_id,alamat,kelurahan,kecamatan,kelompok_id,status,tgl_status,hasil_test,tgl_hasil_test,petugas_id,foto_rumah)
         VALUES(:rek_baru,:rek_lama,:nm_pelanggan,:unit_id,:alamat,:kelurahan,:kecamatan,:kelompok_id,:status,:tgl_status,:hasil_test,:tgl_hasil_test,:petugas_id,:foto_rumah)";
@@ -205,7 +201,6 @@ date_default_timezone_set('Asia/Makassar');
                                     <div class="form-group col-md-6">
                                         <label>Foto Rumah Pelanggan</label>
                                         <input type="file" class="form-control" name="foto_rumah">
-                                        <p>*max file 2mb</p>
                                     </div>
                                 </div>
                                 <button class="btn btn-primary mr-2 my-1" type="submit" name="submit">Simpan</button>
